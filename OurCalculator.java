1614d96850852062d9b6084f80c03f1395613a51
Simon Sehlberg <ssehlbergg@gmail.com>
2018-11-22 16:59:30 +0100

    allt före if-satsen

--- OurCalculator.java
+++ OurCalculator.java
@@ -1,6 +1,25 @@
 import java.util.Scanner;
 public class OurCalculator{
   public static void main(String []args){
-//mjau!
+
+
+      double num1, num2, num3;
+      Scanner scan = new Scanner(System.in);
+
+      System.out.print("Enter first number: ");
+      num1 = scan.nextDouble();
+
+      System.out.print("Enter second number: ");
+      num2 = scan.nextDouble();
+
+      System.out.print("Enter third number or 0 to skip: ");
+      num3 = scan.nextDouble();
+
+      System.out.print("Choose between(+,-,*,/): ");
+      char operator = scan.next().charAt(0);
+
+      double output;
+
+
   }
 }
