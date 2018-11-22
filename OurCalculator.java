9146f9e04d3026d70d233fee5e765983cd832d1c
thella <thellafox@gmail.com>
2018-11-22 17:10:46 +0100

    statements

--- OurCalculator.java
+++ OurCalculator.java
@@ -1,5 +1,32 @@
+import java.util.scanner;
+
 public class OurCalculator{
   public static void main(String []args){
-//mjau!
+//mjau för att se om mr sippish kan komma åt och läsa mina saker jag lägger
+//in
+if (num3 == 0) {
+    switch(operator) {
+      case '+':
+      output = num1 + num2;
+      break;
+
+      case '-':
+      output = num1 - num2;
+      break;
+
+      case '*':
+      output = num1 * num2;
+      break;
+
+      case '/':
+      output = num1 / num2;
+      break;
+
+      default:
+        System.out.printf("You have entered the wrong operator");
+      return;
+      }
+    System.out.println(num1+" "+operator+" "+num2+" = "+output);
   }
 }
+  }
